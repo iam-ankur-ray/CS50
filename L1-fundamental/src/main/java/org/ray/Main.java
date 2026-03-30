512715e700040aa6e407990e21d78292632dd26b
Ankur Ray <143903292+iam-ankur-ray@users.noreply.github.com>
2026-03-30 17:20:57 +0530

    Pyramid problem

--- L1-fundamental/src/main/java/org/ray/Main.java
+++ L1-fundamental/src/main/java/org/ray/Main.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 import static org.ray.BasicCalculator.basicCalculator;
 
 public class Main {
-    public static void main(String[] args) { Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.print("Enter the first digit : ");
-            int firstNumber = sc.nextInt();
-            System.out.print("Enter the second digit : ");
-            int secondNumber = sc.nextInt();
-            System.out.print("Enter the mathematical expression to operate on number +,-,/ or * : ");
-            String operator = sc.next();
-            System.out.println("Your answer is : " + basicCalculator(firstNumber,secondNumber,operator));
+    public static void main(String[] args) {
+        printPyramidOfCertainHeight(5);
+    }
+
+    public static void printPyramidOfCertainHeight(int height){
+        for(int i = 0; i < height; i++){
+            for(int j = height -1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for(int k=0; k <= i; k++){
+                System.out.print("#");
+            }
+            System.out.println();
         }
     }
 }
